e8037ecf02ed35b25b048647bf4d86c009fcb14e
Phuc Proton <phuc.le.it96@gmail.com>
2024-03-14 23:04:33 +0700

    Final retouches to GUI

--- gui.py
+++ gui.py
@@ -2,23 +2,23 @@ import functions
 import PySimpleGUI as gui
 import time
 
-gui.theme("LightBrown9")
-image = gui.Image('fulllogo.png', subsample=7, zoom=2)
+gui.theme("LightBlue")
+image = gui.Image('fulllogo.png', subsample=9, zoom=2)
 
 clock = gui.Text('', key='clock', font=('Helvetica', 12, 'bold'))
-label = gui.Text('Enter a new todo')
-input_box = gui.InputText(tooltip="Enter a new todo", key="todo")
+label = gui.Text('Enter a new todo', font=('Helvetica', 12, 'bold'))
+input_box = gui.InputText(tooltip="Enter a new todo", key="todo", size=(41, 1))
 add_button = gui.Button('Add', tooltip="Add todo")
 list_box = gui.Listbox(values=functions.get_todos(), size=(40, 10), enable_events=True, key='todos')
 edit_button = gui.Button('Edit', tooltip="Edit todo")
 complete_button = gui.Button('Complete', tooltip="Finish a task")
-exit_button = gui.Button('Exit')
+exit_button = gui.Button('Exit', button_color=('white', 'red'))
 
-layout = [[clock, image],
-          [label],
-          [input_box, add_button],
-          [list_box, edit_button, complete_button],
-          [exit_button]]
+col1 = gui.Column([[clock],[image]])
+col2 = gui.Column([[label, add_button, edit_button, complete_button, exit_button], [input_box], [list_box]])
+
+layout = [[col1, col2]]
+
 window = gui.Window('Todo App',
                     layout=layout,
                     margins=(60, 40),
