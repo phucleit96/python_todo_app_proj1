0ad8fb8a455cfdfb218f2dad581c5729a2c5738a
Phuc Proton <phuc.le.it96@gmail.com>
2024-03-14 17:51:14 +0700

    Implement Complete and Exit buttons

--- gui.py
+++ gui.py
@@ -1,15 +1,20 @@
 import functions
 import PySimpleGUI as gui
 
-
 label = gui.Text('Enter a new todo')
 input_box = gui.InputText(tooltip="Enter a new todo", key="todo")
 add_button = gui.Button('Add', tooltip="Add todo")
 list_box = gui.Listbox(values=functions.get_todos(), size=(50, 10), enable_events=True, key='todos')
 edit_button = gui.Button('Edit', tooltip="Edit todo")
+complete_button = gui.Button('Complete', tooltip="Finish a task")
+exit_button = gui.Button('Exit')
 
+layout = [[label],
+          [input_box, add_button],
+          [list_box, edit_button, complete_button],
+          [exit_button]]
 window = gui.Window('Todo App',
-                    layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                    layout=layout,
                     margins=(100, 70),
                     font=('Helvetica', 20))
 while True:
@@ -21,6 +26,7 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value="")
         case "Edit":
             todo_to_edit = values['todos'][0]
             new_todo = values['todo']
@@ -30,8 +36,19 @@ while True:
             todos[index] = new_todo + '\n'
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case 'Complete':
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")
         case 'todos':
             window['todo'].update(value=values['todos'][0])
+        case 'Exit':
+            break
         case gui.WIN_CLOSED:
             break
+
+
 window.close()
