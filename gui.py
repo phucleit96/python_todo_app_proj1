4cc3b7bd330cb9fc13b290078a4ef10c1c90cc10
Phuc Proton <phuc.le.it96@gmail.com>
2024-03-14 20:09:02 +0700

    Fix Bugs using try clauses and add image

--- gui.py
+++ gui.py
@@ -1,24 +1,34 @@
 import functions
 import PySimpleGUI as gui
+import time
 
+gui.theme("LightBrown9")
+image = gui.Image('fulllogo.png', subsample=7, zoom=2)
+
+clock = gui.Text('', key='clock')
 label = gui.Text('Enter a new todo')
 input_box = gui.InputText(tooltip="Enter a new todo", key="todo")
 add_button = gui.Button('Add', tooltip="Add todo")
-list_box = gui.Listbox(values=functions.get_todos(), size=(50, 10), enable_events=True, key='todos')
+list_box = gui.Listbox(values=functions.get_todos(), size=(40, 10), enable_events=True, key='todos')
 edit_button = gui.Button('Edit', tooltip="Edit todo")
 complete_button = gui.Button('Complete', tooltip="Finish a task")
 exit_button = gui.Button('Exit')
 
-layout = [[label],
+layout = [[clock, image],
+          [label],
           [input_box, add_button],
           [list_box, edit_button, complete_button],
           [exit_button]]
 window = gui.Window('Todo App',
                     layout=layout,
                     margins=(100, 70),
-                    font=('Helvetica', 20))
+                    font=('Helvetica', 17))
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=10)
+    window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+    print(1, event)
+    print(2, values)
+    print(3, values['todos'])
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -28,21 +38,27 @@ while True:
             window['todos'].update(values=todos)
             window['todo'].update(value="")
         case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo']
 
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo + '\n'
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo + '\n'
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                gui.popup('Please select an item first!', font=('Helvetica', 17))
         case 'Complete':
-            todo_to_complete = values['todos'][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value="")
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value="")
+            except IndexError:
+                gui.popup('Please select an item first!', font=('Helvetica', 17))
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case 'Exit':
